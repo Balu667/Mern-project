da62199d15fd2c6f3709ba51e8a15139cbb50840
mahendra <balreddy.mahendra@ia.ooo>
2022-08-01 13:17:03 +0530

    [Backend] - small changes don in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -16,12 +16,12 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
   next();
 });
 
